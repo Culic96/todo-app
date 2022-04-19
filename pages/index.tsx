d2604145838ff64af473bcdd4a70739be70b41b6
Nikola Culic <nikola.culic96@gmail.com>
2022-04-19 10:48:00 +0200

    UI updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,43 @@ import type { NextPage } from "next";
 import SignInUp from "../components/SignInUp";
 import styled from "styled-components";
 import Todos from "../components/Todos";
+import SignUpModal from "../components/SignUpModal";
+import LoginModal from "../components/LoginModal";
+import { useState } from "react";
+import { useAuth } from "../Hooks/useAuth";
+import Header from "../components/Header";
 export const Container = styled.div({
   height: "100%",
   width: "100vw",
+  margin: 0,
+  padding: 0,
 });
 
 const Home: NextPage = () => {
+  const [signUpModalOpen, setSignUpModalOpen] = useState(false);
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+  const { auth } = useAuth();
+
   return (
     <div>
+      <Header />
       <Container>
-        <SignInUp />
+        {!auth && (
+          <SignInUp
+            onSignInClick={() => setLoginModalOpen(true)}
+            onSignUpClick={() => setSignUpModalOpen(true)}
+          />
+        )}
         <Todos />
       </Container>
+      <SignUpModal
+        isModalOpen={signUpModalOpen}
+        onClose={() => setSignUpModalOpen(false)}
+      />
+      <LoginModal
+        isModalOpen={loginModalOpen}
+        onClose={() => setLoginModalOpen(false)}
+      />
     </div>
   );
 };
